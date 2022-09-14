4c5bb20e8a1fa5afbefabd7d038d56903b44efb9
Caracole <francoismoulin37@gmail.com>
2022-09-14 14:06:27 +0200

    Object dans array

---
+++ cours/index.js
@@ -0,0 +1,42 @@
+// Rappel des types de données
+let string = "Chaine";
+let number = 25;
+let boolean = true;
+let maVariable; // si rien n'est attribué à la variable comme ici alors le type est 'undefined'
+
+//Tableaux
+let array = ["Bordeaux", "Toulouse", "Nantes"];
+// console.log(array[0][3]);
+
+let array2 = ["Bordeaux", 24, true, [1, 2], { nom: "Denis" }];
+// console.log(array2[4].nom);
+
+let objet = {
+  pseudo: "Denis",
+  age: 33,
+  technos: ["Javascript", "React", "NodeJs"],
+  admin: false,
+};
+
+let data = [
+  {
+    pseudo: "Denis",
+    age: 33,
+    technos: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+
+  {
+    pseudo: "Denis",
+    age: 33,
+    technos: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+
+  {
+    pseudo: "Denis",
+    age: 33,
+    technos: ["Javascript", "React", "NodeJs"],
+    admin: false,
+  },
+];
