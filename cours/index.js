9e038eeb6a3411dcf289cc2d7d2f38e1862498b6
Caracole <francoismoulin37@gmail.com>
2022-09-15 13:59:26 +0200

    avancement boucle for

--- cours/index.js
+++ cours/index.js
@@ -27,16 +27,55 @@ let data = [
   },
 
   {
-    pseudo: "Denis",
-    age: 33,
-    technos: ["Javascript", "React", "NodeJs"],
+    pseudo: "Ralph",
+    age: 30,
+    technos: ["CSS", "React", "NodeJs"],
     admin: false,
   },
 
   {
-    pseudo: "Denis",
-    age: 33,
-    technos: ["Javascript", "React", "NodeJs"],
-    admin: false,
+    pseudo: "Edouard",
+    age: 52,
+    technos: ["Php", "React", "NodeJs"],
+    admin: true,
   },
 ];
+
+// console.log(data[2].pseudo);
+
+// --------------------------
+// Les structures de controle
+// --------------------------
+
+// if else
+
+if (data[0].age > data[1].age) {
+  //   console.log(data[0].pseudo + " est plus agé que " + data[1].pseudo);
+} else {
+  // valeur si faux
+}
+
+// while
+
+let w = 0;
+
+while (w < 10) {
+  w++;
+  //   console.log("La valeur de w est de : " + w);
+}
+
+// Do while
+
+let d = 0;
+
+do {
+  d++;
+  //   console.log(d);
+} while (d < 5);
+
+// Les boucles for
+
+for (const user of data) {
+  document.body.innerHTML +=
+    "<li>" + user.pseudo + " - " + user.age + " ans" + "</li>";
+}
