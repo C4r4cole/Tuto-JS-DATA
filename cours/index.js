1b4e5975503ef9b7b3c4440ae02faab63b300a97
Caracole <francoismoulin37@gmail.com>
2022-09-17 17:34:10 +0200

    until fonctions récursives

--- cours/index.js
+++ cours/index.js
@@ -76,6 +76,32 @@ do {
 // Les boucles for
 
 for (const user of data) {
-  document.body.innerHTML +=
-    "<li>" + user.pseudo + " - " + user.age + " ans" + "</li>";
+  //   document.body.innerHTML +=
+  //     "<li>" + user.pseudo + " - " + user.age + " ans" + "</li>";
 }
+
+// console.log(data.length);
+
+// (1) on déclare la valeur de i / (2) jusqu'ou on boucle / (3) incrémentation de i jusqu'à ce que la condition 2 soit remplie
+for (i = 0; i < data.length; i++) {
+  // console.log(i);
+  // console.log(data[i]);
+  // document.body.innerHTML += "<h2>" + data[i].pseudo + "</h2>";
+}
+
+document.body.addEventListener("click", (e) => {
+  // console.log(e.target.id);
+  switch (e.target.id) {
+    case "javascript":
+      document.body.style.background = "yellow";
+      break;
+    case "php":
+      document.body.style.background = "violet";
+      break;
+    case "python":
+      document.body.style.background = "blue";
+      break;
+    default:
+      null;
+  }
+});
